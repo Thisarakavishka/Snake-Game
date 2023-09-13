5517ca628163c97ed951c5c688b831b673f0f787
thisarakavishka <thisarakavishka0@gmail.com>
2023-09-13 16:36:26 +0530

    Create Direction For Key Input in keyboard

--- src/main/java/com/example/snakegame/controller/GameFormController.java
+++ src/main/java/com/example/snakegame/controller/GameFormController.java
@@ -1,10 +1,17 @@
 package com.example.snakegame.controller;
 
+import com.example.snakegame.util.Direction;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.AnchorPane;
 
-public class GameFormController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GameFormController implements Initializable {
 
     @FXML
     private AnchorPane root;
@@ -12,4 +19,32 @@ public class GameFormController {
     @FXML
     private Canvas canvas;
 
+    private Direction direction = Direction.LEFT;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        root.sceneProperty().addListener((observableValue, oldScene, newScene) -> {
+            if(newScene != null){
+                setDirectionForKeyCode(newScene);
+            }
+        });
+    }
+
+    private void setDirectionForKeyCode(Scene scene) {
+        scene.setOnKeyPressed(keyEvent -> {
+            if(keyEvent.getCode() == KeyCode.UP && direction != Direction.DOWN){
+                direction = Direction.UP;
+                System.out.println(direction);
+            } else if (keyEvent.getCode() == KeyCode.DOWN && direction != Direction.UP) {
+                direction = Direction.DOWN;
+                System.out.println(direction);
+            } else if (keyEvent.getCode() == KeyCode.LEFT && direction != Direction.RIGHT) {
+                direction = Direction.LEFT;
+                System.out.println(direction);
+            } else if (keyEvent.getCode() == KeyCode.RIGHT && direction != Direction.LEFT) {
+                direction = Direction.RIGHT;
+                System.out.println(direction);
+            }
+            //sanke can go up ,right ,down ,left, but he's once going to up he can't go to down because he cross his body
+        });
+    }
 }
