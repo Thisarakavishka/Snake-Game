a9dc6984fbff44acf5b0641e4630aa600dcc44f2
thisarakavishka <thisarakavishka0@gmail.com>
2023-09-15 21:36:25 +0530

    Add Dimensions & Refactor GameFormController

--- src/main/java/com/example/snakegame/controller/GameFormController.java
+++ src/main/java/com/example/snakegame/controller/GameFormController.java
@@ -1,12 +1,16 @@
 package com.example.snakegame.controller;
 
+import com.example.snakegame.util.Dimentions;
 import com.example.snakegame.util.Direction;
+import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -20,13 +24,47 @@ public class GameFormController implements Initializable {
     private Canvas canvas;
 
     private Direction direction = Direction.LEFT;
+
+    private int foodX;
+
+    private int foodY;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
+        generateFood();
+        new AnimationTimer(){
+            private long lastUpdate = 0;
+            @Override
+            public void handle(long now) {
+                if(now - lastUpdate >= 100_000_000){
+                    update();
+                    draw(graphicsContext);
+                    lastUpdate = now;
+                }
+            }
+        }.start();
+
         root.sceneProperty().addListener((observableValue, oldScene, newScene) -> {
             if(newScene != null){
                 setDirectionForKeyCode(newScene);
             }
         });
+    }
+
+    private void generateFood() {
+        foodX = (int) (Math.random() * (Dimentions.WIDTH - Dimentions.SQUARE_SIZE) / Dimentions.SQUARE_SIZE) * Dimentions.SQUARE_SIZE;
+        foodY = (int) (Math.random() * (Dimentions.HEIGHT - Dimentions.SQUARE_SIZE) / Dimentions.SQUARE_SIZE) * Dimentions.SQUARE_SIZE;
+    }
+
+    private void draw(GraphicsContext graphicsContext) {
+        // Background Colour
+        graphicsContext.setFill(Color.RED);
+        graphicsContext.fillRect(0,0,Dimentions.WIDTH,Dimentions.HEIGHT);
+
+    }
+
+    private void update() {
     }
 
     private void setDirectionForKeyCode(Scene scene) {
@@ -44,7 +82,7 @@ public class GameFormController implements Initializable {
                 direction = Direction.RIGHT;
                 System.out.println(direction);
             }
-            //sanke can go up ,right ,down ,left, but he's once going to up he can't go to down because he cross his body
+            //snake can go up ,right ,down ,left, but he's once going to up he can't go to down because he cross his body.
         });
     }
 }
--- src/main/java/com/example/snakegame/util/Dimentions.java
+++ src/main/java/com/example/snakegame/util/Dimentions.java
@@ -3,4 +3,5 @@ package com.example.snakegame.util;
 public class Dimentions {
     public static final int HEIGHT = 600;
     public static final int WIDTH = 800;
+    public static final int SQUARE_SIZE = 20;
 }
