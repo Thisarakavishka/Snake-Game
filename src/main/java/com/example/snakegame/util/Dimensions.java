8ea9cbfb95ed7501f1f07f88d0c24d156f695d3a
thisarakavishka <thisarakavishka0@gmail.com>
2023-09-20 19:50:05 +0530

    Refactor Dimensions.java

--- src/main/java/com/example/snakegame/util/Dimensions.java
+++ src/main/java/com/example/snakegame/util/Dimensions.java
@@ -1,6 +1,6 @@
 package com.example.snakegame.util;
 
-public class Dimentions {
+public class Dimensions {
     public static final int HEIGHT = 600;
     public static final int WIDTH = 800;
     public static final int SQUARE_SIZE = 20;
