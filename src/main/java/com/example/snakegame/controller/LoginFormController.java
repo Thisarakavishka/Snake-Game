0e36ceae5bccfa5aa02e33cd295f500ce4e20934
thisarakavishka <thisarakavishka0@gmail.com>
2023-09-21 06:26:33 +0530

    Add LoginFormController

---
+++ src/main/java/com/example/snakegame/controller/LoginFormController.java
@@ -0,0 +1,21 @@
+package com.example.snakegame.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginFormController {
+
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    private JFXButton btnStart;
+
+    @FXML
+    void btnStartOnAction(ActionEvent event) {
+
+    }
+
+}
