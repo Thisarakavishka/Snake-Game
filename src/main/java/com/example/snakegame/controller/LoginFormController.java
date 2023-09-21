0b21486f54007abd73e6208769a7f097ddfc845c
thisarakavishka <thisarakavishka0@gmail.com>
2023-09-21 06:33:30 +0530

    Refactor LoginFormController

--- src/main/java/com/example/snakegame/controller/LoginFormController.java
+++ src/main/java/com/example/snakegame/controller/LoginFormController.java
@@ -3,7 +3,12 @@ package com.example.snakegame.controller;
 import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginFormController {
 
@@ -15,7 +20,14 @@ public class LoginFormController {
 
     @FXML
     void btnStartOnAction(ActionEvent event) {
-
+        try {
+            Stage stage = (Stage) root.getScene().getWindow();
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/com/example/snakegame/view/GameForm.fxml"))));
+            stage.setTitle("Snake Game");
+            stage.centerOnScreen();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
