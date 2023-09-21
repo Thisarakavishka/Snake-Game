076038112b62f15022e1b4004849842252eac242
thisarakavishka <thisarakavishka0@gmail.com>
2023-09-22 02:44:00 +0530

    Implementing methods in GameOverFormController

--- src/main/java/com/example/snakegame/controller/GameOverFormController.java
+++ src/main/java/com/example/snakegame/controller/GameOverFormController.java
@@ -1,4 +1,53 @@
 package com.example.snakegame.controller;
 
-public class GameOverFormController {
+import com.jfoenix.controls.JFXButton;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GameOverFormController implements Initializable {
+
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    private JFXButton btnExit;
+
+    @FXML
+    private JFXButton btnPlay;
+
+    private Stage stage;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Platform.runLater(() -> {
+            stage = (Stage) root.getScene().getWindow();
+        });
+    }
+
+    @FXML
+    void btnExitOnAction(ActionEvent event) {
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/com/example/snakegame/view/LoginForm.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @FXML
+    void btnPlayOnAction(ActionEvent event) {
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/com/example/snakegame/view/GameForm.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
